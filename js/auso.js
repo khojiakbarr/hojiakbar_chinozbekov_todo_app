b0dfe32dc191dd659a1753707d70b2eaba723a0f
khojiakbarr <chinozbekov09@gmail.com>
2024-01-20 08:43:28 +0500

    first commit

---
+++ js/auso.js
@@ -0,0 +1,12 @@
+const registerForm = document.querySelector("#registerForm");
+const loginForm = document.querySelector("#loginForm");
+
+loginForm.addEventListener('submit',(event) => {
+  event.preventDefault();
+
+})
+registerForm.addEventListener('submit',(event) => {
+  event.preventDefault();
+  
+
+})
